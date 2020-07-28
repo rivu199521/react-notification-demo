3fbd80401a722e73e6c1aacde1b1bc9d5afe046a
unknown <mukherjeerivu7@gmail.com>
2020-07-28 20:58:25 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
 
+toast.configure()
+
+const CustomToast = ({ closeToast }) => {
+  return (
+    <div>
+      Something went wrong! <button onClick={closeToast}>Close</button>
+    </div>
+  )
+}
 function App() {
+
+  const notify = () => {
+    toast('Default!', { position: toast.POSITION.TOP_LEFT })
+   
+    toast.success('Success!', {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 8000
+    })
+
+    toast.info('Info!', {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose: false
+    })
+    toast.warn(<CustomToast/>, {
+      position: toast.POSITION.BOTTOM_LEFT
+    })
+
+    toast.error('Error!', {position: toast.POSITION.BOTTOM_CENTER})
+
+    toast('Wow so easy!', { position: toast.POSITION.BOTTOM_RIGHT})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={notify}>Notify!</button>
     </div>
   );
 }
