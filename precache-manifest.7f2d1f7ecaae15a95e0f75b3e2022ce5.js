4afd7395fe79f0cf4e95f22797c8921839fe40a2
unknown <mukherjeerivu7@gmail.com>
2020-07-28 21:03:49 +0530

    Updates

---
+++ precache-manifest.7f2d1f7ecaae15a95e0f75b3e2022ce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c08f5c6f8ce0d82365eb0ac2a521533",
+    "url": "/react-notification-demo/index.html"
+  },
+  {
+    "revision": "6f21af979bd2a672a284",
+    "url": "/react-notification-demo/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6123cab92b4a5ce4a975",
+    "url": "/react-notification-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f21af979bd2a672a284",
+    "url": "/react-notification-demo/static/js/2.aa116c33.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-notification-demo/static/js/2.aa116c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6123cab92b4a5ce4a975",
+    "url": "/react-notification-demo/static/js/main.c24db5ed.chunk.js"
+  },
+  {
+    "revision": "89c9aff66eec609a40fc",
+    "url": "/react-notification-demo/static/js/runtime-main.58f00f9e.js"
+  }
+]);
